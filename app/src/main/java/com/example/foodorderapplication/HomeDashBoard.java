4ab45489ca746672ce368b587d4ed2d51f41b746
PavanKumarReddySama50559251 <pavankum.sama@smail.astate.edu>
2020-01-22 00:28:35 -0600

    Home page banner with CardView

---
+++ app/src/main/java/com/example/foodorderapplication/HomeDashBoard.java
@@ -0,0 +1,14 @@
+package com.example.foodorderapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeDashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_dash_board);
+    }
+}
